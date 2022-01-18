7d701014cb017925ee3c8dd7e38dc55c4ec321a8
Matty <53088136+byronbytes@users.noreply.github.com>
2022-01-18 11:20:33 -0500

    fixes

--- dFilter/Form1.cs
+++ dFilter/Form1.cs
@@ -17,19 +17,23 @@ namespace dFilter {
     }
 
     public static string directory;
-    private void button1_Click(object sender, EventArgs e) {
+    private void button1_Click(object sender, EventArgs e) 
+    {
       string directory = "";
       FolderBrowserDialog fbd = new FolderBrowserDialog();
 
-      if (fbd.ShowDialog() == DialogResult.OK) {
+      if (fbd.ShowDialog() == DialogResult.OK) 
+      {
         directory = fbd.SelectedPath;
         textBox1.Text = directory;
         var filesEx = Directory.GetFiles(directory, "*.*", SearchOption.TopDirectoryOnly).Where(s => s.EndsWith(".exe"));
 
-        foreach(FileInfo FilesE in filesEx) {
+        foreach(FileInfo FilesE in filesEx)  // this also errors. will fix.
+        {
           string PathDir = FilesE.FullName;
           listView1.Items.Add(PathDir + Environment.NewLine);
         }
+        
       }
     }
 
@@ -37,19 +41,25 @@ namespace dFilter {
       string desktopDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\dFilter\\";
       // This creates a folder on the desktop to move all of the detected files into.
 
-      if (Directory.Exists(desktopDir)) {
+      if (Directory.Exists(desktopDir)) 
+      {
         MessageBox.Show("Unable to create a folder on the desktop since it already exists.");
-      } else {
+      } 
+      else 
+      {
         Directory.CreateDirectory(desktopDir);
       }
 
     }
 
     public static void DeleteFiles() {
-      try {
+      try 
+      {
         Directory.Delete(desktopDir); // this code errors, will fix later.
         MessageBox.Show("The files inside of the dFilter output folder have been deleted.");
-      } catch {
+      } 
+      catch 
+      {
         MessageBox.Show("An error occurred.");
       }
 
